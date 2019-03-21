52823828e97c4ddbc0dc70921b312c7ec4c51dc1
Henry Huang <Mchighlight@gmail.com>
2019-03-22 00:21:50 +0800

    basic unit test

--- rfw_practice/accounts/tests.py
+++ rfw_practice/accounts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+#dyamic function
+def create_user(**params):
+    """create new user"""
+    return get_user_model().objects.create_user(**params)
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.user = create_user(
+            email='test@gmail.com',
+            username='test',
+            password='testpass',
+        )
+
+    def test_created_user(self):
+        qs = User.objects.filter(username='test')
+        self.assertEqual(qs.count(), 1)
--- rfw_practice/status/tests.py
+++ rfw_practice/status/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from .models import Status
+
+User = get_user_model()
+#dyamic function
+def create_user(**params):
+    """create new user"""
+    return get_user_model().objects.create_user(**params)
+
+class StatusTestCase(TestCase):
+    def setUp(self):
+        self.user = create_user(
+            email='test@gmail.com',
+            username='test',
+            password='testpass',
+        )
+
+    def test_creating_status(self):
+        user = User.objects.get(username='test')
+        obj = Status.objects.create(user=user, content="HH")
+        self.assertEqual(obj.id, 1)
+        qs = Status.objects.all()
+        self.assertEqual(qs.count(),1)
